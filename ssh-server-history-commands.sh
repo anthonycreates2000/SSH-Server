91e2d0fbc6244dfdc20be519ca3960efce522276
anthonycreates2000 <anthonyoktavius1110@gmail.com>
2023-07-03 14:32:17 +0700

    Uploaded ssh server history commands file for initializing ssh server.

---
+++ ssh-server-history-commands.sh
@@ -0,0 +1,78 @@
+# Note: Change sample_user to any user you'd like!
+# This process happens afer we add a sample user from the previous bash script (encrypt-user-data.sh).
+sudo systemctl status sshd.service
+ip addr
+ssh sample_user@172.23.174.71
+ssh -p 2000 -i ~/.ssh/sample_user_submission_id_rsa sample_user@172.23.174.71
+ssh -p 22 sample_user@172.23.174.71
+ssh -p 2000 sample_user@172.23.174.71
+cd ..
+cd
+ls
+cd ..
+ls
+cd sample_user
+sudo cd sample_user
+su cd sample_user
+sudo cd sample_user
+sudo -s cd sample_user
+cd sample_user
+cd anthony_max/
+cat ~/.ssh/
+ls
+ls --all
+cd .ssh
+cd ..
+ssh sample_user@172.23.174.71
+sudo apt-get purge openssh-client
+sudo apt-get install openssh-client
+ssh sample_user@172.23.174.71
+sudo apt-get purge openssh-client
+ssh sample_user@172.23.174.71
+dpkg --listfiles openssh-client
+su sample_user
+sudo netstat
+sudo netstat -npt
+sudo netplan apply
+sudo apt-get install openssh-server
+ssh sample_user@172.23.174.71
+cd .ssh
+ls
+ssh-keygen
+cd ..
+cd ..
+ssh sample_user@172.23.174.71
+journalctl -u ssh.service
+cd .ssh
+ls --all
+ssh-copy-id -i ~/.ssh/sample_user_submission_id_rsa.pub sample@172.23.174.71
+ssh-copy-id -i ~/.ssh/sample_user_submission_id_rsa.pub sample_user@172.23.174.71
+ssh sample_user@172.23.174.71
+ip addr
+ssh -p 20 sample_user@172.23.174.71
+ssh sample_user@172.23.174.71
+sudo netplan apply
+ssh sample_user@172.23.174.71
+sudo systemctl restart sshd.service
+ssh sample_user@172.23.174.71
+cd ..
+ssh sample_user@172.23.174.71
+sudo systemctl restart sshd.service
+sudo nano /etc/ssh/sshd_config
+
+# Apply test cases for our SSH Server.
+ssh -i ~/.ssh/sample_user_submission_id_rsa sample_user@172.23.174.71
+ssh -i -p 2000 ~/.ssh/sample_user_submission_id_rsa sample_user@172.23.174.71
+ssh -p 2000 -i ~/.ssh/sample_user_submission_id_rsa sample_user@172.23.174.71
+ssh -p 2000 -i ~/.ssh/sample_user_submission_id_rsa root@172.23.174.71
+ssh -p 22 -i ~/.ssh/sample_user_submission_id_rsa root@172.23.174.71
+ssh -p 2000 -i root@172.23.174.71
+ssh -p 2000 root@172.23.174.71
+ssh -p 2000 sample_user@172.23.174.71
+ssh -p 2000 -i ~/.ssh/sample_user_submission_id_rsa root@172.23.174.71
+ssh -p 2000 -i ~/.ssh/sample_user_submission_id_rsa sample_user@172.23.174.71
+sudo journalctl -b -u ssh -o json > log-ssh.json
+ssh -p 2000 -i ~/.ssh/sample_user_submission_id_rsa sample_user@172.23.174.71
+ssh -p 2000 -i ~/.ssh/sample_user_submission_id_rsa root@172.23.174.71
+ssh -p 2000 root@172.23.174.71
+ssh -p 22 -i ~/.ssh/sample_user_submission_id_rsa root@172.23.174.71
